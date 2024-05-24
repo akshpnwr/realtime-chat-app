2a06e491de456242e1da0b3c1f736462542f273c
akshpnwr <akashpanwar9976@gmail.com>
2024-05-25 00:57:51 +0530

    feature: signup, login and logout

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,12 +1,19 @@
 import { BiLogOut } from "react-icons/bi";
-// import useLogout from "../../hooks/useLogout";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
-  //   const { loading, logout } = useLogout();
+  const { loading, logout } = useLogout();
 
   return (
     <div className="mt-auto">
-      <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
+      {loading ? (
+        <span className="loading loading-spinner"></span>
+      ) : (
+        <BiLogOut
+          onClick={logout}
+          className="w-6 h-6 text-white cursor-pointer"
+        />
+      )}
     </div>
   );
 };
---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
+
+const useSignup = () => {
+    const [loading, setLoading] = useState(false);
+    const { setAuthUser } = useAuthContext();
+
+    const signup = async ({ fullname, username, password, confirmPassword, gender }) => {
+        const success = handleInputError({ fullname, username, password, confirmPassword, gender })
+        if (!success) return
+        try {
+            setLoading(true)
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ fullname, username, password, confirmPassword, gender })
+            })
+
+            const data = await res.json();
+
+            if (data.error) throw new Error(data.error)
+
+            localStorage.setItem('chat-user', JSON.stringify(data))
+            setAuthUser(data)
+        } catch (error) {
+            toast.error(error.message)
+        }
+        finally {
+            setLoading(false)
+        }
+    };
+
+    return { loading, signup }
+}
+
+const handleInputError = ({ fullname, username, password, confirmPassword, gender }) => {
+    console.log(fullname, username, password, confirmPassword, gender);
+    if (!fullname || !username || !password || !confirmPassword || !gender) {
+        toast.error('Please fill in all fields')
+        return false
+    }
+
+    if (password !== confirmPassword) {
+        toast.error('Passwords do not match')
+        return false
+    }
+
+    if (password.length < 6) {
+        toast.error('Password must be at least 6 characters long')
+        return false
+    }
+    return true
+}
+
+export default useSignup
