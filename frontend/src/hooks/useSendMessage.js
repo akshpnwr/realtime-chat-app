825475020ba96758caa7c164c651c43df349e5c1
akshpnwr <akashpanwar9976@gmail.com>
2024-05-29 12:00:29 +0530

    send message funtionality added

---
+++ frontend/src/hooks/useSendMessage.js
@@ -0,0 +1,33 @@
+import toast from 'react-hot-toast'
+import useConversation from '../zustand/useConversation';
+import { useState } from 'react';
+
+const useSendMessage = () => {
+    const [loading, setLoading] = useState(false);
+    const { selectedConversation } = useConversation();
+
+    console.log(selectedConversation);
+    const sendMessage = async (message) => {
+        setLoading(true)
+        try {
+            if (message === "") throw new Error('Message cannot be empty')
+            await fetch(`api/message/send/${selectedConversation._id}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ message })
+            })
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+    return { loading, sendMessage }
+}
+
+export default useSendMessage
